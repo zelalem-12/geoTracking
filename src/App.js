85436ff13ca48a7d5493cfc28d43649985002ee3
zelalem-12 <zelalem.antigegn12@gmail.com>
2021-06-15 09:31:09 +0300

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="heder">Geo tracking web applications</h1>
+      <p>Welcome to the device trackiing up</p>
+    </div>
+  );
+}
+
+export default App;
